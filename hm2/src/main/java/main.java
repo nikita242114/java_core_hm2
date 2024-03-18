97ae6eb019c4be8fb2c8f683231e97753130c299
nikita242114 <5358711@mail.ru>
2024-03-19 01:24:18 +0500

    work7

--- hm2/src/main/java/main.java
+++ hm2/src/main/java/main.java
@@ -4,19 +4,21 @@ import static tasks.task1.countEvens;
 import static tasks.task2.elementDifference;
 import static tasks.task3.zeroElement;
 
-public static void main(String[] args) {
-    System.out.println("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=");
-    System.out.println("Привет! Сейчас тебе будет предложено ввести несколько значений. Пожалуйста введи их.");
+public class main {
+    public static void main(String[] args) {
+        System.out.println("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=");
+        System.out.println("Привет! Сейчас тебе будет предложено ввести несколько значений. Пожалуйста введи их.");
 
-    Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-    System.out.print("Желаемая длина массива: ");
-    int lengthArray = scanner.nextInt();
+        System.out.print("Желаемая длина массива: ");
+        int lengthArray = scanner.nextInt();
 
-    countEvens(new int[lengthArray]);
+        countEvens(new int[lengthArray]);
 
-    elementDifference(new int[lengthArray]);
+        elementDifference(new int[lengthArray]);
 
-    zeroElement(new int[lengthArray]);
+        zeroElement(new int[lengthArray]);
 
+    }
 }
